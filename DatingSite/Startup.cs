2f1ededb97fdfce74a6806ac6b0816791f23a7a8
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2020-04-14 12:27:11 +0200

    update

--- DatingSite/Startup.cs
+++ DatingSite/Startup.cs
@@ -1,8 +1,12 @@
+using System.Net;
 using System.Text;
 using DatingSite.Data;
+using DatingSite.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -44,6 +48,24 @@ namespace DatingSite
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                        {
+                            context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                            if (error != null)
+                            {
+                                context.Response.AddApplicationError(error.Error.Message);
+                                await context.Response.WriteAsync(error.Error.Message);
+                            }
+                            
+                        });
+                });
             }
 
             // app.UseHttpsRedirection();
